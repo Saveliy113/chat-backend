c09a612fe06ca6e44793647575ff524eef668248
Saveliy <sava2698@gmail.com>
2023-06-21 09:45:04 +0600

    Rooms and messages gateways added

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -3,10 +3,11 @@ import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { messagesProvider } from './messages.providers';
+import { MessagesGateway } from './messages.gateway';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [MessagesController],
-  providers: [...messagesProvider, MessagesService],
+  providers: [...messagesProvider, MessagesService, MessagesGateway],
 })
 export class MessagesModule {}
--- src/rooms/rooms.module.ts
+++ src/rooms/rooms.module.ts
@@ -4,10 +4,11 @@ import { RoomsController } from './rooms.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { roomsProvider } from './rooms.providers';
 import { usersProvider } from 'src/users/users.providers';
+import { RoomsGateway } from './rooms.gateway';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [RoomsController],
-  providers: [...roomsProvider, ...usersProvider, RoomsService],
+  providers: [...roomsProvider, ...usersProvider, RoomsService, RoomsGateway],
 })
 export class RoomsModule {}
